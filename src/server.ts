37d82186cf14f9f13c8ad9e4191da1cfbb6b40b8
Noel Bryant <noelsobryant@gmail.com>
2024-08-08 19:49:20 +0300

    Fix eslint errors.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import winston from 'winston';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -25,9 +26,10 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI || '')
+mongoose
+  .connect(process.env.MONGO_URI || '')
   .then(() => logger.info('MongoDB connected'))
-  .catch(err => logger.error(err));
+  .catch((err) => logger.error(err));
 
 // Routes
 app.use('/api/v1/products', productRoutes);
